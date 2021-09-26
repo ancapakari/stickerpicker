313442f459b6382d08d57c32a5497679dd00e61b
ancapakari <77232145+ancapakari@users.noreply.github.com>
2021-09-26 00:49:36 -0400

    added hug

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'f9ade452'
+version = '0.1.0+dev.f9ade452'
+linkified_version = '0.1.0+dev.[f9ade452](https://github.com/maunium/stickerpicker/commit/f9ade452967bacb0db8339fe793a96c6404eddb7)'
